1141f98f2b0bda360b08f82c235b7b4a7ecef494
leahk <lkoenigs@student.touro.edu>
2026-02-16 20:38:35 -0500

    feat: Implemented Home and About routes with template inheritance

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():  # put application's code here
+    return render_template('home.html')
 
+@app.route('/about')
+def about():  # put application's code here
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run()
