8ec0ec21be84af19f470440231ad1dea48aa223f
leahk <lkoenigs@student.touro.edu>
2026-02-18 23:06:45 -0500

    fix: Final link integration and code cleanup

--- app.py
+++ app.py
@@ -7,26 +7,26 @@ projects_data = [
      "title": "Portfolio Site",
      "description": "Built with Flask",
      "tech": "Python, HTML, CSS, Jinja2",
-     "github_link": "[https://github.com/](https://github.com/)...",
-     "image_url": "[https://picsum.photos/id/1/600/400](https://picsum.photos/id/1/600/400)"},
+     "github_link": "https://github.com/lkoenigs-2025/HW-1.5",
+     "image_url": "https://picsum.photos/seed/picsum/200/300"},
     {"id": 2,
      "title": "Weather App",
      "description": "API Integration",
      "tech": "JavaScript, React, OpenWeatherMap API",
-     "github_link": "[https://github.com/](https://github.com/)...",
-     "image_url": "[https://picsum.photos/id/10/600/400](https://picsum.photos/id/10/600/400)"},
+     "github_link": "https://github.com/lkoenigs-2025/HW-1.5",
+     "image_url": "https://picsum.photos/200"},
     {"id": 3,
      "title": "To-Do List",
      "description": "Task Management",
-     "tech": "JavaScript, React, OpenWeatherMap API",
-     "github_link": "[https://github.com/](https://github.com/)...",
-     "image_url": "[https://picsum.photos/id/10/600/400](https://picsum.photos/id/10/600/400)"},
+     "tech": "JavaScript, Python, CSS, API",
+     "github_link": "https://github.com/lkoenigs-2025/HW-1.5",
+     "image_url": "https://picsum.photos/200"},
     {"id": 4,
      "title": "Simple Calculator",
      "description": "Math and Logic",
-     "tech": "JavaScript, React, OpenWeatherMap API",
-     "github_link": "[https://github.com/](https://github.com/)...",
-     "image_url": "[https://picsum.photos/id/10/600/400](https://picsum.photos/id/10/600/400)"}
+     "tech": "Java, React, JavaScript CSS",
+     "github_link": "https://github.com/lkoenigs-2025/HW-1.5",
+     "image_url": "https://picsum.photos/200/300/?blur"}
 ]
 @app.route('/')
 def index():  # put application's code here
@@ -37,16 +37,17 @@ def about():  # put application's code here
     return render_template('about.html')
 
 @app.route('/projects')
-def projects(projects_data):   # put application's code here
+def projects():   # put application's code here
     return render_template('projects.html', projects = projects_data)
 
 @app.route('/project/<int:project_id>')
 def project_detail(project_id):
+    selected_project = None
     for project in projects_data:
         if project['id'] == project_id:
             selected_project = project
-        if not selected_project:
-            abort(404)
+    if not selected_project:
+        abort(404)
     return render_template('projects_detail.html', project = selected_project)
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
